bd1a323c1ae98bbe97926d5c53f3a57bec87d5fb
Mateus Heck <mateussheck@gmail.com>
2025-04-10 12:55:54 -0300

    New Text List Behaviour

--- src/Components/TextList.tsx
+++ src/Components/TextList.tsx
@@ -8,30 +8,32 @@ function renderListItem(
   addItem: (id: string, rowIndex: number, value: string) => void
 ) {
   return (
-    <ul id="text-list-unordered-list">
-      {
-        layoutItem.data?.at(0)?.map((value, index) => (
-          <li key={index} id="text-list-item">
-            { // Renders a Checkbox or Textarea
-              useCheckbox(layoutItem, value, index, undefined, updateItem, "text-list-item-data", (layoutItem.data?.at(0)?.length === index + 1 ? "focus-item-" + layoutItem.i : ""))
-            }
-            <span
-              id="text-list-item-remove"
-              className="remove-button"
-              hidden={layoutItem.static}
-              onMouseDown={
-                (e) => {
-                  e.stopPropagation();
-                  removeItem(layoutItem.i, 0, index);
-                }}
-            >
-              &times;
-            </span>
-          </li>
-        ))
-      }
-      {layoutItem.static ? null : renderAddItem(layoutItem, addItem)}
-    </ul>
+    <>
+      <ul id="text-list-unordered-list">
+        {
+          layoutItem.data?.at(0)?.map((value, index) => (
+            <li key={index} id="text-list-item">
+              { // Renders a Checkbox or Textarea
+                useCheckbox(layoutItem, value, index, undefined, updateItem, "text-list-item-data", (layoutItem.data?.at(0)?.length === index + 1 ? "focus-item-" + layoutItem.i : ""))
+              }
+              <span
+                id="text-list-item-remove"
+                className="remove-button"
+                hidden={layoutItem.isLocked}
+                onMouseDown={
+                  (e) => {
+                    e.stopPropagation();
+                    removeItem(layoutItem.i, 0, index);
+                  }}
+              >
+                &times;
+              </span>
+            </li>
+          ))
+        }
+      </ul>
+      {layoutItem.isLocked ? null : renderAddItem(layoutItem, addItem)}
+    </>
   )
 }
 
@@ -40,25 +42,23 @@ function renderAddItem(
   addItem: (id: string, rowIndex: number, value: string) => void
 ) {
   return (
-    <li id="text-list-item">
-      <textarea
-        className="text-list-add-item-area"
-        id="text-list-item-input"
-        value=""
-        placeholder="Add New Item"
-        onChange={(e) => {
-          e.stopPropagation();
-          addItem(layoutItem.i, 0, e.target.value);
-          setTimeout(() => {
-            const newItem = document.getElementsByClassName("focus-item-" + layoutItem.i)[0] as HTMLTextAreaElement;
-            if (newItem) {
-              newItem.focus(); // Set focus to the new item
-              newItem.setSelectionRange(newItem.value.length, newItem.value.length); // Set cursor to the end
-            }
-          }, 0);
-        }}
-      />
-    </li>
+    <textarea
+      className="text-list-add-item-area"
+      id="text-list-item-input"
+      value=""
+      placeholder="Add New Item"
+      onChange={(e) => {
+        e.stopPropagation();
+        addItem(layoutItem.i, 0, e.target.value);
+        setTimeout(() => {
+          const newItem = document.getElementsByClassName("focus-item-" + layoutItem.i)[0] as HTMLTextAreaElement;
+          if (newItem) {
+            newItem.focus(); // Set focus to the new item
+            newItem.setSelectionRange(newItem.value.length, newItem.value.length); // Set cursor to the end
+          }
+        }, 0);
+      }}
+    />
   )
 }
 
